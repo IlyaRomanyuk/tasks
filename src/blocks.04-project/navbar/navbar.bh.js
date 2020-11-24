17afc3f88d30507e6ad2922c44ea812f54dc847d
Shuklin Artem <a.shuklin@intervolga.ru>
2020-11-24 13:47:09 +0400

    rename on navbar

--- src/blocks.04-project/navbar/navbar.bh.js
+++ src/blocks.04-project/navbar/navbar.bh.js
@@ -1,13 +1,10 @@
 module.exports = function(bh) {
     bh.match('navbar', function(ctx, json) {
-        let linkArr = [...new Array(5)];
         ctx.tag('div'),
-            linkArr.map((value, index) => {
-                ctx.content({
-                    tag: 'a',
-                    elem: 'link',
-                    content: `link ${index}`,
-                });
+            ctx.content({
+                tag: 'a',
+                elem: 'link',
+                content: `link`,
             });
     }, true);
 };
