4d017ba8f55ab03f422dc73145a9d1cadc9ce461
Shuklin Artem <a.shuklin@intervolga.ru>
2020-11-25 10:31:06 +0400

    transparentBorder complite

---
+++ src/bundles/transparentBorder.bemjson.js
@@ -0,0 +1,9 @@
+module.exports = {
+  block: 'page',
+  title: 'transparentBorder',
+  content: [
+    {block: 'transparent', content: [
+        {elem: 'text', content: ('Lorem it is some text')},
+      ]},
+  ],
+};
